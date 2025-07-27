994ad93caa9e50001e6e432c2f1c69b5953f5b8a
Basile Mouret <Basile-Mouret@users.noreply.github.com>
2025-07-27 22:37:16 +0200

    added home manager

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,11 +1,28 @@
 {
-  description = "A very basic flake";
+  description = "Basile's NixOS Flake";
+
   inputs = {
+    # NixOS official package source
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    # Home Manager
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      # Make sure home-manager's nixpkgs input follows the one from your system
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { self, nixpkgs }: {
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
     nixosConfigurations.zephyrus = nixpkgs.lib.nixosSystem {
-      modules = [ ./configuration.nix];
+      system = "x86_64-linux";
+      specialArgs = { inherit home-manager; }; # Pass home-manager to your modules
+      modules = [
+        ./configuration.nix
+
+        # Add the home-manager module to your system
+        home-manager.nixosModules.default
+      ];
     };
   };
 }
