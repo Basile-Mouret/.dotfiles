aeb6afa6fe8148398810e7a20997873104c6e565
Basile Mouret <Basile-Mouret@users.noreply.github.com>
2025-07-27 12:37:37 +0200

    remove absolute file path

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -7,7 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      /etc/hardware-configuration.nix
+      ./hardware-configuration.nix
     ];
     nix.settings.experimental-features = ["nix-command" "flakes"];
 
