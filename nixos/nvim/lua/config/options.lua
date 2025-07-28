5a8357de24f6b341276f95da13ad186945a73fc0
Basile Mouret <Basile-Mouret@users.noreply.github.com>
2025-07-28 19:31:56 +0200

    updated nvim options and keybinds

--- nixos/nvim/lua/config/options.lua
+++ nixos/nvim/lua/config/options.lua
@@ -1,11 +1,14 @@
 -- ~/.config/nvim/lua/config/options.lua
+vim.g.mapleader = " "
 
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.wrap = true
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
 vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.opt.softtabstop = 2
+vim.o.wrap = true
+vim.o.autoindent = true
 vim.o.smartindent = true
 vim.o.smartcase = true
 vim.o.ignorecase = true
@@ -13,5 +16,13 @@ vim.o.hlsearch = false
 vim.o.signcolumn = 'yes'
 vim.o.undofile = true -- Persistent undo
 vim.o.updatetime = 300 -- Faster completion and LSP updates
+vim.o.clipboard = "unnamedplus"
 vim.o.termguicolors = true -- Enable true colors
+
+-- Transparent background
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "none" })
 
